497610f6fabea77a82a122cfdd98ae6fb9ba8387
Suraj Kandpal <surajkandpal@northcorp.tech>
2020-08-11 17:15:39 +0530

    push code for the first time

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HomePage from "./pages/homepage/hompage.jsx";
+import { Route, Switch } from "react-router-dom";
+import ShopPage from "./components/shop/shop.jsx";
 
+import Header from "./components/header/header.jsx";
+import "./App.css";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/shop" component={SignInAndSignUpPage} />
+      </Switch>
     </div>
   );
 }
